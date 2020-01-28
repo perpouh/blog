3b700cb54fcff9bd8ebf7101a39b13e59e4acfc6
perpouh <perpouh@gmail.com>
2020-01-29 00:20:59 +0900

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    sidebar: [
+      [ '/', 'Home'],
+      ['/resume/', '人物'],
+      ['/vuepress/', 'VuePress' ],
+    ],
+    sidebarDepth: 0
+  }
+}
